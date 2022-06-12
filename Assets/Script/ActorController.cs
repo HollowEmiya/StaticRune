6dd7b828d516fc2cb8e816b9953b520929960db5
HollowEmiya <73419954+HollowEmiya@users.noreply.github.com>
2022-06-12 23:43:00 +0800

    playerInput

--- Assets/Script/ActorController.cs
+++ Assets/Script/ActorController.cs
@@ -6,14 +6,22 @@ public class ActorController : MonoBehaviour
 {
     public GameObject model;
     public PlayerInput pi;
+    public float walkSpeed = 1.35f;
 
     [SerializeField]            // 让变量可见，该变量必须被编译器支持
     private Animator anim;
+    private Rigidbody rigid;
+    private Vector3 movingVec;      // move direction vector for fixedUpdate
 
     private void Awake()
     {
         pi = GetComponent<PlayerInput>();           // get by itself
         anim = model.GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+        //if(rigid == null)
+        //{
+
+        //}
     }
 
     // Start is called before the first frame update
@@ -23,10 +31,23 @@ public class ActorController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update()                               // update for scene to display 60 times pre second
     {
-        anim.SetFloat("forward", pi.Dmag );
-        if(pi.Dvec.magnitude > 0.01f )
+        anim.SetFloat("forward", pi.Dmag * (pi.run ? 2.0f : 1.0f));
+        if(pi.Dvec.magnitude > 0.01f)
+        {
             anim.transform.forward = pi.Dvec;
+            movingVec = pi.Dmag * model.transform.forward * walkSpeed * (pi.run ? 2.0f : 1.0f);
+        }
+    }
+
+    private void FixedUpdate()              // Physics engine simulates 50 times pre second, it has a different speed fron update
+    {
+        // rigid.position += movingVec * Time.fixedDeltaTime;    // different update, must fix deltaTime
+        rigid.velocity = new Vector3(movingVec.x, rigid.velocity.y, movingVec.z);
+    }
+    void playerMove()
+    {
+
     }
 }
--- Assets/Script/PlayerInput.cs
+++ Assets/Script/PlayerInput.cs
@@ -6,16 +6,29 @@ public class PlayerInput : MonoBehaviour
 {
     // sometime we need change input-key but input-manager can't
     // we need the input defined by ourself
+    [Header("===== Key Settings =====")]
     public string keyUp = "w";            
     public string keyDown = "s";            
     public string keyLeft = "a";
-    public string keyRight = "d";            
+    public string keyRight = "d";
 
+    public string keyA;
+    public string keyB;
+    public string keyC;
+    public string keyD;
+
+    [Header("===== Output Siganl =====")]
     public float Dup;
     public float Dright;
     public float Dmag;
     public Vector3 Dvec;
 
+    // 1.pressing signal
+    public bool run;
+    // 2. trigger once type siganl
+    // 3. double trigger
+
+    [Header("===== Others =====")]
     public bool inputEnable = true;
 
     private float targetDup;
@@ -39,6 +52,7 @@ public class PlayerInput : MonoBehaviour
         targetDup = (Input.GetKey(keyUp) ? 1.0f : 0)  - (Input.GetKey(keyDown) ? 1.0f : 0);
         targetDright = (Input.GetKey(keyRight) ? 1.0f : 0) - (Input.GetKey(keyLeft) ? 1.0f : 0);
         
+        // player can't input
         if(!inputEnable)
         {
             targetDup = 0;
@@ -47,10 +61,16 @@ public class PlayerInput : MonoBehaviour
 
         Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, 0.1f);
         Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);
-        
+
+
+        //Vector2 tmp = new Vector2(Dup,Dright);
+        //tmp.Normalize();
+        //Dmag = tmp.magnitude > 0.01f ? tmp.magnitude : 0;
+        //Debug.Log(tmp.magnitude);
         Dmag = Mathf.Sqrt(Dup * Dup + Dright * Dright);
         Dvec = Dright * transform.right + Dup * transform.forward;
         //Debug.Log("Dup: " + Dup);
 
+        run = Input.GetKey(keyA);
     }
 }
